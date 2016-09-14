2cb72d4252cc60ccadb8364b619faec24ff3bd56
Kyle Torpey <kyletorpey@users.noreply.github.com>
2016-09-13 23:28:24 -0400

    Update bitcoin_experts.py

--- bitcoin_experts.py
+++ bitcoin_experts.py
@@ -25,9 +25,7 @@ def comments_bot():
 			tweet_body = str(comment.author) + ": " + comment.body
 			tweet_body = tweet_body[:115]
 			tweet = tweet_body + " " + comment.permalink
-			if ((comment.subreddit_id not in btcsubreddits) or (comment.created_utc < launchtime)) or (comment.id in comments_cache):
-				print "New comment by " + str(comment.author) + ", but it's not in a Bitcoin subreddit or old."
-			else:
+			if (comment.subreddit_id in btcsubreddits) and (comment.created_utc > launchtime) and (comment.id not in comments_cache):
 				print "New tweet: " + tweet
 				api.update_status(tweet)
 				comments_cache.append(comment.id)
@@ -37,10 +35,8 @@ def posts_bot():
 		for post in posts:
 			tweet_body = str(post.author) + ": " + post.title
 			tweet_body = tweet_body[:115]
-			tweet = tweet_body + " " + post.url
-			if ((post.subreddit_id not in btcsubreddits) or (post.created_utc < launchtime)) or (post.id in posts_cache):
-				print "New submission by " + str(post.author) + ", but it's not in a Bitcoin subreddit or old."
-			else:
+			tweet = tweet_body + " " + post.permalink
+			if (post.subreddit_id in btcsubreddits) and (post.created_utc > launchtime) and (post.id not in posts_cache):
 				print "New tweet: " + tweet
 				api.update_status(tweet)
 				posts_cache.append(post.id)
@@ -48,7 +44,7 @@ def posts_bot():
 while True:
 	posts_bot()
 	comments_bot()
-	time.sleep(60)
+	time.sleep(120)
 
 ###TO DO LIST###
 #add posts to the mailing list by bitcoin experts?
